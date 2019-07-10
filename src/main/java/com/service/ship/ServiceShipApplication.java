c26a214be7f6d58be82bb6cdf26ba3f323a4b266
phuongnguyencodeengine <phuong.nguyen@codeenginestudio.com>
2019-07-10 13:38:11 +0700

    Init project

---
+++ src/main/java/com/service/ship/ServiceShipApplication.java
@@ -0,0 +1,13 @@
+package com.service.ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceShipApplication.class, args);
+	}
+
+}
